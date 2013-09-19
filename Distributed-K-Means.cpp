9908c8cde08ed9e67e39a2843c109b0813b352e3
Shoutmon <hlqs1234@qq.com>
2013-09-20 03:21:23 +0800

    code

--- Distributed-K-Means.cpp
+++ Distributed-K-Means.cpp
@@ -3,9 +3,11 @@
 #include <cstring>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 
 using namespace std;
 
+const double INF=1e20;
 const int MAXD = 1000;//最高维度数
 const int MAXN = 1000;//最大样本点数
 const int MAXC = 50;//类的最大个数
@@ -26,12 +28,24 @@ class D_K_Means
     int Point_Num;//样本数
     int Point_Dimension;//样本属性维度
 
+
     public:
-    bool ReadData();//读取数据
+    bool ReadData();//读取初始数据
     void Init();//初始化K类的中心
+    void Mapper();
     void Calc();//算法主运算程序
-    void Writ();//结果写入文件
+    void TempWrit();//将一轮迭代结束后的结果写入临时文件
 };
+
+double D_K_Means::Distance(int p, int c)//编号为p的点与第c类的中心的距离
+{
+    double dis=0;
+    for(int j=0;j<Point_Dimension;j++)
+    {
+        dis+=(Point[p][j]-Cluster[c].Center[j])*(Point[p][j]-Cluster[c].Center[j]);
+    }
+    return sqrt(dis);
+}
 
 bool D_K_Means::ReadData()//读取数据
 {
@@ -46,6 +60,8 @@ bool D_K_Means::ReadData()//读取数据
             cin>>Point[i][j];//读取第i个样本点的第j个属性
         }
     }
+    Init();//初始化K个类的中心
+    TempWrit();//将所有类的中心作为第一轮迭代前的数据写入临时文件
 }
 
 void D_K_Means::Init()//初始化K个类的中心
@@ -62,37 +78,84 @@ void D_K_Means::Init()//初始化K个类的中心
     }
 }
 
-void D_K_Means::Calc()
+void D_K_Means::Mapper()
+{
+    FILE *fp;
+    if((fp=fopen("TempData.txt","r"))==NULL)
+    {
+        cout<<"Open TempData.txt Error!"<<endl;
+        exit(0);
+    }
+    double temp;
+    for(int i=0;i<Cluster_Num;i++)//读取中心
+    {
+        Cluster[i].Number=0;
+        for(int j=0;j<Point_Dimension;j++)
+        {
+            cin>>temp;
+            Cluster[i].Center[j]=temp;
+        }
+    }
+    for(int i=0;i<Point_Num;i++)//重新计算所有类中的样本点
+    {
+        int index;
+        double dis=INF;
+        for(int j=0;j<Cluster_Num;j++)
+        {
+            if(Distance(i,j)<dis)
+            {
+                dis=Distance(i,j);
+                index=j;
+            }
+        }
+        Cluster[index].Member[Number++]=i;
+    }
+}
+
+void D_K_Means::Combiner()
 {
 
 }
 
-void D_K_Means::Writ()
+
+void D_K_Means::Reducer()
 {
 
+}
+
+void D_K_Means::TempWrit()//将所有类的中心写入临时文件
+{
+    FILE *fp;
+    if((fp=fopen("TempData.txt","w"))==NULL)
+    {
+        cout<<"Open TempData.txt Error!"<<endl;
+        exit(0);
+    }
+    for(int i=0;i<Cluster_Num;i++)
+    {
+        for(int j=0;j<Point_Dimension;j++)
+        {
+            cout<<Cluster[i].Center[j];
+            if(j!=Point_Dimension-1) cout<<" ";
+            else cout<<endl;
+        }
+    }
+    fclose(fp);
 }
 
 void Solve(D_K_Means kmeans)
 {
     kmeans.ReadData();
-    kmeans.Init();
-    kmeans.Calc();
-    kmeans.Writ();
+    kmeans.Mapper();
+    kmeans.Conbiner();
+    kmeans.Reducer();
 }
 
 int main()
 {
-    if(freopen("data.txt","r",stdin)==false)
-    {
-        cout<<"Open data.txt Error!"<<endl;
-        exit(0);
-    }
-    freopen("result.txt","w",stdout);
 
     D_K_Means kmeans;
     Solve(kmeans);//算法主体过程
 
-    fclose(stdin);
-    fclose(stdout);
     return 0;
 }
